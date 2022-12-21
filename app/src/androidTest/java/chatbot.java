5da4c41978235700298cb8ab12dec862982ad77a
mane <emmanuel_mr@tesch.edu.mx>
2022-12-20 21:09:49 -0600

    agrego

---
+++ app/src/androidTest/java/chatbot.java
@@ -0,0 +1,11 @@
+import android.app.Application;
+
+import com.androidnetworking.AndroidNetworking;
+
+public class chatbot extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AndroidNetworking.initialize(getApplicationContext());
+    }
+}
